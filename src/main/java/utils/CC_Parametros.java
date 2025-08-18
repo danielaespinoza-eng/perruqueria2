223c8937359d1d4030f26032d9fd06fcb4eecbb2
danielaespinoza-eng <daniela.espinoza@testgroup.cl>
2025-08-18 15:59:59 -0400

    camnbio ruta csv

--- src/main/java/utils/CC_Parametros.java
+++ src/main/java/utils/CC_Parametros.java
@@ -1,3 +1,26 @@
+/*package main.java.utils;
+
+import java.io.File;
+
+public interface CC_Parametros {
+    String url = "https://franciscavalenzuelasalazar.github.io/mascopet/catalog.html";
+
+    String nombreAutomatizador = "[Flujo-Automatizado]";
+    String nombreProyecto = "[*Proyecto*]";
+
+    String gloDir = "C:" + File.separator + "Users" + File.separator + "TG" + File.separator +
+            "eclipse-workspace" + File.separator + "Estructura-Base-New_Perruqueria" + File.separator +
+            "CC_Logs_Perruqueria";
+    
+    
+
+    int gloColumnas = 2;
+    String gloNombreCSV = "CSVParametersCarritoCompras.csv";
+
+    default String getRutaCSV() {
+        return gloDir + File.separator + "data" + File.separator + gloNombreCSV;
+    }
+}*/
 package main.java.utils;
 
 import java.io.File;
@@ -8,12 +31,8 @@ public interface CC_Parametros {
     String nombreAutomatizador = "[Flujo-Automatizado]";
     String nombreProyecto = "[*Proyecto*]";
 
-    //String gloDir = "C:" + File.separator + "Users" + File.separator + "TG" + File.separator +
-      //      "eclipse-workspace" + File.separator + "Estructura-Base-New_Perruqueria" + File.separator +
-            //"CC_Logs_Perruqueria";
-    
-   // String gloDir =System.getProperty("user.dir");
-    String gloDir = new File("").getAbsolutePath();
+    // Ruta relativa al workspace
+    String gloDir = "CC_Logs_Perruqueria";
 
     int gloColumnas = 2;
     String gloNombreCSV = "CSVParametersCarritoCompras.csv";
